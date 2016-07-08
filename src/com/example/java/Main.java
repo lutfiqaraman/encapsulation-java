46f25cd5e463c7661c5e35d36a56abb64fdff019
Lutfi Qaraman <lutfiqaraman@gmail.com>
2016-07-08 22:29:31 +0300

    Update Main.java

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -5,21 +5,27 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-
+        /* Generate a random number */
         Random num1 = new Random();
         int number1 = num1.nextInt(100) + 1;
 
+        /* Generate a random number */
         Random num2 = new Random();
         int number2 = num2.nextInt(100) + 1;
 
+        /* Print the numbers */
         Calculate.doPrintNumbers(number1,number2);
 
+        /* adding two numbers */
         Calculate.Sum(number1, number2);
 
+        /* Subtracting two numbers */
         Calculate.Subtraction(number1, number2);
-
+        
+        /* Multiplying two numbers */
         Calculate.Multiply(number1,number2);
 
+        /* Dividing two numbers */
         Calculate.Divide(number1,number2);
         
     }
